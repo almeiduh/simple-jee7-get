4a85b29ff3df7e76c1f619f4f1117c89a0abdf40
Diogo Almeida <diogo.almeida@criticaltechworks.com>
2018-12-06 17:19:05 +0000

    Added tests

---
+++ src/test/java/org/javaee7/jaxrs/simple/get/LeTest.java
@@ -0,0 +1,14 @@
+package org.javaee7.jaxrs.simple.get;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LeTest {
+
+    @Test
+    public void bestTestEver() {
+        // assert statements
+        assertEquals(2, 1 + 1, "1 + 1 is 2");
+    }
+}
